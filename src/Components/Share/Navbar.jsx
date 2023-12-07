6eb80c5f1c85d6802bbab064b380fe6e3de3dff4
Kamrul-Official <kamrul.manzur@gmail.com>
2023-12-08 00:16:28 +0600

    update nav

--- src/Components/Share/Navbar.jsx
+++ src/Components/Share/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-green-900 rounded-box w-52 text-white"
           >
             {navLinks?.map((item) => (
               <Link key={item.id} to={item.path}>
