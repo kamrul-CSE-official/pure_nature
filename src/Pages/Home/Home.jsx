de203fa48e9d3a6729823d08216e14eacc6e3498
Kamrul-Official <kamrul.manzur@gmail.com>
2023-12-08 00:13:00 +0600

    Added react Helmet package

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { Helmet } from "react-helmet";
+
 import { AuthContext } from "../../Providers/AuthProvider";
 
 export default function Home() {
@@ -9,6 +11,11 @@ export default function Home() {
   };
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pure Nature | Home</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
       Home : {user?.email}
       <button onClick={handleLogOut} className="btn btn-error">
         LogOut
