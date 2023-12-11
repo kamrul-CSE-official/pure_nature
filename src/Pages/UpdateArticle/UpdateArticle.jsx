5212a6709519e3f1eed9863d9c69d870889cb803
Kamrul-Official <kamrul.manzur@gmail.com>
2023-12-11 12:31:52 +0600

    update article done

--- src/Pages/UpdateArticle/UpdateArticle.jsx
+++ src/Pages/UpdateArticle/UpdateArticle.jsx
@@ -1,18 +1,20 @@
-import moment from "moment";
 import { useContext, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Swal from "sweetalert2";
 import axios from "axios";
+import moment from "moment";
 
 export default function UpdateArticle() {
   const data = useLoaderData();
   const { user } = useContext(AuthContext);
   const [articleData, setArticleData] = useState({
-    title: "",
-    content: "",
-    img: "",
+    title: data?.article?.title,
+    content: data?.article?.content,
+    img: data?.article?.img,
   });
+
+  const navegate = useNavigate();
 
   const [contentError, setContentError] = useState("");
 
@@ -25,7 +27,7 @@ export default function UpdateArticle() {
   };
 
   const validateContent = () => {
-    const content = articleData.content || ""; // Ensure content is defined
+    const content = articleData.content || "";
     const wordCount = content.trim().split(/\s+/).length;
 
     if (wordCount < 300) {
@@ -37,57 +39,56 @@ export default function UpdateArticle() {
     }
   };
 
-  const handleUpdate = (e) => {
+  const handleUpdate = async (e) => {
     e.preventDefault();
 
     if (!validateContent()) {
       return;
     }
+    const apiUrl = "process.env.REACT_APP_API_URL";
 
     articleData.date = moment().format("MMMM D, YYYY");
     articleData.author = user.name;
     articleData.email = user.email;
     articleData.authorImg = user.img;
-    console.log("Article submitted:", articleData);
-    axios
-      .post(
-        `${import.meta.env.VITE_SERVERapi}/articlesUpdate/${
-          data?.article?._id
-        }`,
-        articleData
-      )
-      .then((res) => {
-        if (res.data.insertedId && res.data.success) {
-          Swal.fire({
-            position: "top-end",
-            icon: "success",
-            title: "Article has been created.",
-            showConfirmButton: false,
-            timer: 1500,
-          });
-        }
-      })
-      .catch((error) => {
-        // Handle error if the request fails
-        console.error("Error submitting article:", error);
-        Swal.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Something went wrong! Please try again.",
-        });
-      });
 
-    // Reset form fields
+    try {
+      const res = await axios.patch(
+        `${apiUrl}/articlesUpdate/${data?.article?._id}`,
+        articleData
+      );
+      if (res?.data.acknowledged && res?.data.modifiedCount > 0) {
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Article has been successfully updated.",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+      navegate(`/articleDetails/${data?.article?._id}`);
+    } catch (error) {
+      console.error("Error submitting article:", error);
+      const errorMessage =
+        error.response?.data?.message || "Something went wrong!";
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: errorMessage,
+      });
+    }
+
     setArticleData({
       title: "",
-      //   content: "",
       img: "",
     });
   };
+
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Update Article</h1>
       <form onSubmit={handleUpdate} className="mx-auto">
+        {/* Title input */}
         <div className="mb-4">
           <label
             htmlFor="title"
@@ -105,6 +106,8 @@ export default function UpdateArticle() {
             required
           />
         </div>
+
+        {/* Content textarea */}
         <div className="mb-4">
           <label
             htmlFor="content"
@@ -125,6 +128,8 @@ export default function UpdateArticle() {
           ></textarea>
           {contentError && <p className="text-red-500">{contentError}</p>}
         </div>
+
+        {/* Image URL input */}
         <div className="mb-4">
           <label
             htmlFor="img"
@@ -141,13 +146,17 @@ export default function UpdateArticle() {
             className="mt-1 p-2 border rounded w-full input input-bordered input-accent text-area"
           />
         </div>
+
+        {/* Update button */}
         <button
           type="submit"
-          className="bg-green-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          className="btn bg-green-500 text-white py-2 px-4 rounded hover:bg-blue-600"
         >
           Update Article
         </button>
       </form>
+
+      {/* Article details display */}
       <div className="my-5 border-3">
         <p>
           Author: <span className="font-bold">{user?.name}</span>
